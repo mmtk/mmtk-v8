0743e025bf12a225e7591b685c8c4172fd367215
Javad Amiri <javad.amiri@anu.edu.au>
2020-11-04 15:45:07 +1100

    debug CI

--- .github/scripts/get-v8.sh
+++ .github/scripts/get-v8.sh
@@ -2,7 +2,10 @@ set -xe
 
 # clean-up the previously created V8 directories
 cd $V8_ROOT
+rm -rf v8
 rm -rf depot_tools
+rm -f .gclient
+rm -f .gclient_entries
 
 # clone the V8/Chromium depot tools and add them to path
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
@@ -11,5 +14,5 @@ export PATH=$V8_ROOT/depot_tools:$PATH
 
 # fetch v8 and update dependencies
 gclient
-rm -rf v8
 fetch v8
+gclient sync

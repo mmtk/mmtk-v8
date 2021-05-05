2ab3aaa1cdae6f73121afd491e2accdf6fc4c636
Javad Amiri <javad.amiri@anu.edu.au>
2021-05-05 11:33:48 +1000

    update preprare_mutator arguments (#26)
    * update preprare_mutator arguments

--- mmtk/src/collection.rs
+++ mmtk/src/collection.rs
@@ -39,7 +39,11 @@ impl Collection<V8> for VMCollection {
         }
     }
 
-    fn prepare_mutator<T: MutatorContext<V8>>(_tls: OpaquePointer, _m: &T) {
+    fn prepare_mutator<T: MutatorContext<V8>>(
+        _tls_worker: OpaquePointer,
+        _tls_mutator: OpaquePointer,
+        _m: &T,
+    ) {
         unimplemented!()
     }
 }

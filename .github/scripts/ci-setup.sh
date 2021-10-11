4284bbfbce8d9e67ecaaf06e1f513cbfc46db3f7
Yi Lin <qinsoon@gmail.com>
2021-10-11 15:22:31 +1100

    Run tests on github hosted runners

--- .github/scripts/ci-setup.sh
+++ .github/scripts/ci-setup.sh
@@ -1,3 +1,8 @@
 set -xe
 
-# Currently no setup is required
+# Install nightly rust
+rustup toolchain install $RUSTUP_TOOLCHAIN
+rustup target add i686-unknown-linux-gnu --toolchain $RUSTUP_TOOLCHAIN
+rustup component add clippy --toolchain $RUSTUP_TOOLCHAIN
+rustup component add rustfmt --toolchain $RUSTUP_TOOLCHAIN
+rustup override set $RUSTUP_TOOLCHAIN

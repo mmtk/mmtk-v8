164e7f3884d6a229571a870c9726ff990dd328e0
Yi Lin <qinsoon@gmail.com>
2022-01-21 10:29:03 +1300

    Fix depot_tools version in our CI script (#51)
    * Checkout depot_tools version after gclient sync

--- .github/scripts/get-v8.sh
+++ .github/scripts/get-v8.sh
@@ -21,3 +21,6 @@ fetch v8
 git -C v8 checkout $V8_VERSION
 
 gclient sync
+# Set depot_tools to the given revision.
+# This needs to be done after gclient sync. Otherwise gclient sync will reset depot_tools to HEAD
+git -C depot_tools checkout $DEPOT_TOOLS_VERSION

5e3b8132249f4f817762dd55f3298be43876f590
Yi Lin <qinsoon@gmail.com>
2022-09-21 01:56:52 +0000

    Use --tag to install cross

--- .github/scripts/build-mmtk.sh
+++ .github/scripts/build-mmtk.sh
@@ -8,7 +8,7 @@ if ! [ -x "$(command -v cross)" ]; then
   echo 'Cross is not available. Will install cross.' >&2
   # lock to version 0.2.4. This version uses libc 2.31 for x86_64.
   # This matches the version in the sysroot used by V8 (debian bullseye)
-  cargo install cross@0.2.4 --git https://github.com/cross-rs/cross
+  cargo install cross --tag v0.2.4 --git https://github.com/cross-rs/cross
 fi
 
 # simply build mmtk-v8 with nogc

e46cecd69d2bd4ad5096010f18a32ae5f1cf786a
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2023-08-13 23:23:46 +1000

    CI: Fix rustup

--- .github/scripts/build-mmtk.sh
+++ .github/scripts/build-mmtk.sh
@@ -3,5 +3,8 @@ set -xe
 . $(dirname "$0")/common.sh
 
 # simply build mmtk-v8 with nogc
-cd $THE_ROOT/mmtk
-rustup run $RUSTUP_TOOLCHAIN cargo build --features nogc
+rustup toolchain install $RUSTUP_TOOLCHAIN
+rustup target add i686-unknown-linux-gnu --toolchain $RUSTUP_TOOLCHAIN
+rustup component add clippy --toolchain $RUSTUP_TOOLCHAIN
+rustup component add rustfmt --toolchain $RUSTUP_TOOLCHAIN
+rustup override set $RUSTUP_TOOLCHAIN

b7e37ea51cbc63c1972c41f147f0cdcf662fdfe6
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2023-08-14 09:59:25 +1000

    Revert debug changes

--- .github/scripts/build-mmtk.sh
+++ .github/scripts/build-mmtk.sh
@@ -4,7 +4,7 @@ set -xe
 
 # simply build mmtk-v8 with nogc
 rustup toolchain install $RUSTUP_TOOLCHAIN
-rustup target add i686-unknown-linux-gnu --toolchain $RUSTUP_TOOLCHAIN
+rustup target add x86_64-unknown-linux-gnu --toolchain $RUSTUP_TOOLCHAIN
 rustup component add clippy --toolchain $RUSTUP_TOOLCHAIN
 rustup component add rustfmt --toolchain $RUSTUP_TOOLCHAIN
 rustup override set $RUSTUP_TOOLCHAIN

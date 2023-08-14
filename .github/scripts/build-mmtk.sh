ce19fa5bceb5e30b721263d1a6a974b04b3d9f70
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2023-08-14 12:04:14 +1000

    Update rustup setups

--- .github/scripts/build-mmtk.sh
+++ .github/scripts/build-mmtk.sh
@@ -3,10 +3,7 @@ set -xe
 . $(dirname "$0")/common.sh
 
 # simply build mmtk-v8 with nogc
-rustup toolchain install $RUSTUP_TOOLCHAIN
-rustup target add x86_64-unknown-linux-gnu --toolchain $RUSTUP_TOOLCHAIN
-rustup component add clippy --toolchain $RUSTUP_TOOLCHAIN
-rustup component add rustfmt --toolchain $RUSTUP_TOOLCHAIN
+rustup toolchain install $RUSTUP_TOOLCHAIN --target x86_64-unknown-linux-gnu --component clippy rustfmt
 rustup override set $RUSTUP_TOOLCHAIN
 
 cd $THE_ROOT/mmtk && cargo build --features nogc
